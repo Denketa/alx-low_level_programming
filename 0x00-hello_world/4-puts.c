f77d50147d2f451ffd6338e4da0683f5a3cceae9
keter <dketer05@gmail.com>
2023-03-21 08:49:05 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,8 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-
- int main(void)
+int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
